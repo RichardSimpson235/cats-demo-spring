b39805ae3511a92bd77f12c6715f69be5c2ec3f4
RichardSimpson235 <57775052+RichardSimpson235@users.noreply.github.com>
2022-03-17 11:53:08 -0400

    Implement CatController

---
+++ src/main/java/com/revature/controllers/CatController.java
@@ -0,0 +1,24 @@
+package com.revature.controllers;
+
+import com.revature.models.Cat;
+import com.revature.services.CatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CatController {
+
+    @Autowired
+    private CatService catService;
+
+    @GetMapping("/cats/{id}")
+    public ResponseEntity<Cat> getCatById(@PathVariable("id") int id) {
+        return ResponseEntity.of(catService.findById(id));
+    }
+
+    @PostMapping("/cats/")
+    public Cat createCat(@RequestBody Cat cat) {
+        return catService.save(cat);
+    }
+}
